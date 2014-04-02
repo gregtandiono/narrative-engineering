12d8c346562d354f1721c39979fde86b84d46d70
gregtandiono <gregtandiono@gmail.com>
2014-04-02 02:10:19 -0700

    added scroll effect

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,11 +1,13 @@
 (function() {
   $(window).scroll(function() {
       var scroll = $(window).scrollTop();
-      if (scroll >= 300) {
+      if (scroll >= 100) {
           $("header").addClass("is-scrolled");
       }
       else if (scroll <= 200) {
         $("header").removeClass("is-scrolled");
       }
+      opacityVal = (scroll / 250.0);
+      $('.home-hero.is-scrolled').css('opacity', opacityVal);
   });
 })();
